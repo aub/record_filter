8c18e087b284b9b28a857289988add71d3b8fbfe
Mat Brown <mat@patch.com>
2009-04-13 17:53:53 -0400

    Use #to_positive_sql/#to_negative_sql for negation logic

--- lib/record_filter/restrictions.rb
+++ lib/record_filter/restrictions.rb
@@ -2,42 +2,61 @@ module RecordFilter
   module Restrictions
     class Base
       def initialize(column_name, value, options={})
-        @column_name, @value = column_name, value
-        @negated = options[:negated]
+        @column_name, @value, @negated = column_name, value, !!options.delete(:negated)
       end
 
       def to_conditions
         @value.nil? ? [to_sql] : [to_sql, @value]
       end
+
+      def to_sql
+        @negated ? to_negative_sql : to_positive_sql
+      end
+
+      def to_negative_sql
+        "!(#{to_positive_sql})"
+      end
     end
 
     class EqualTo < Base
-      def to_sql
-        "#{@column_name} #{'!' if @negated}= ?"
+      def to_positive_sql
+        "#{@column_name} = ?"
+      end
+
+      def to_negative_sql
+        "#{@column_name} != ?"
       end
     end
 
     class IsNull < Base
-      def to_sql
-        "#{@column_name} IS #{'NOT ' if @negated}NULL"
+      def to_positive_sql
+        "#{@column_name} IS NULL"
+      end
+
+      def to_negative_sql
+        "#{@column_name} IS NOT NULL"
       end
     end
 
     class LessThan < Base
-      def to_sql
-        @negated ? "!(#{@column_name} < ?)" : "#{@column_name} < ?"
+      def to_positive_sql
+        "#{@column_name} < ?"
       end
     end
 
     class GreaterThan < Base
-      def to_sql
-        @negated ? "!(#{@column_name} > ?)" : "#{@column_name} > ?"
+      def to_positive_sql
+        "#{@column_name} > ?"
       end
     end
 
     class In < Base
-      def to_sql
-        "#{@column_name} #{'NOT ' if @negated}IN (?)"
+      def to_positive_sql
+        "#{@column_name} IN (?)"
+      end
+
+      def to_negative_sql
+        "#{@column_name} NOT IN (?)"
       end
     end
 
