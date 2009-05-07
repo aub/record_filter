d102172c82ed4eacad230206447194cd2413849a
Aubrey Holland <aubrey@gmail.com>
2009-05-07 10:57:06 -0400

    get rid of ruby-debug in a more elegant manner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,12 @@ require 'rubygems'
 gem 'rspec', '~> 1.1'
 gem 'sqlite3-ruby'
 
+begin
+  require 'ruby-debug'
+rescue LoadError
+  puts 'ruby-debug is not available. Good luck debugging'
+end
+
 require 'spec'
 
 require File.join(File.dirname(__FILE__), '..', 'lib', 'record_filter')
